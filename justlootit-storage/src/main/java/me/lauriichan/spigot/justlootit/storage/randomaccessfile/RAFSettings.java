e50dc561c786364ace77eacd5ddb63347f831708
Lauriichan <laura.endress@syntaxphoenix.com>
2023-02-26 11:11:32 +0100

    Set min amount of bits used for valueIdBits

--- justlootit-storage/src/main/java/me/lauriichan/spigot/justlootit/storage/randomaccessfile/RAFSettings.java
+++ justlootit-storage/src/main/java/me/lauriichan/spigot/justlootit/storage/randomaccessfile/RAFSettings.java
@@ -4,10 +4,10 @@ package me.lauriichan.spigot.justlootit.storage.randomaccessfile;
  * Settings for the {@link RAFStorage}
  */
 public final class RAFSettings {
-    
+
     public static final int DEFAULT_VALUES_PER_FILE = 1024;
     public static final int DEFAULT_COPY_BUFFER_BYTES = 64;
-    
+
     public static final RAFSettings DEFAULT = new RAFSettings(DEFAULT_VALUES_PER_FILE, DEFAULT_COPY_BUFFER_BYTES);
 
     public static final int LOOKUP_AMOUNT_SIZE = Short.BYTES;
@@ -28,6 +28,8 @@ public final class RAFSettings {
     public final int lookupHeaderSize;
 
     /**
+     * Creates a new RAFSettings instance
+     * 
      * @param valuesPerFile   the amount of values that should fit into on file
      *                            (will be rounded up to the next power of two if
      *                            this isn't a power of two)
@@ -37,7 +39,7 @@ public final class RAFSettings {
      */
     public RAFSettings(int valuesPerFile, int copyBufferBytes) {
         this.copyBufferSize = Math.max(copyBufferBytes * 1024, 1024);
-        this.valueIdBits = Integer.bitCount(valuesPerFile - 1);
+        this.valueIdBits = Math.max(Integer.bitCount(valuesPerFile - 1), 1);
         this.valueIdMask = -1 >> (Integer.SIZE - valueIdBits);
         this.valueIdAmount = valueIdMask + 1;
         this.lookupHeaderSize = valueIdAmount * LOOKUP_ENTRY_SIZE + LOOKUP_AMOUNT_SIZE;
