fb678e19bb4b30102111611054ab97b98f9e4c2c
Lauriichan <lauriichan@playuniverse.org>
2023-04-21 03:25:21 +0200

    Fixed ItemStack IO

--- justlootit-nms-1_19_R3/src/main/java/me/lauriichan/spigot/justlootit/nms/v1_19_R3/io/ItemStackIO1_19_R3.java
+++ justlootit-nms-1_19_R3/src/main/java/me/lauriichan/spigot/justlootit/nms/v1_19_R3/io/ItemStackIO1_19_R3.java
@@ -3,19 +3,17 @@ package me.lauriichan.spigot.justlootit.nms.v1_19_R3.io;
 import java.lang.invoke.MethodHandle;
 
 import org.bukkit.craftbukkit.v1_19_R3.inventory.CraftItemStack;
-import org.bukkit.craftbukkit.v1_19_R3.util.CraftMagicNumbers;
 
 import me.lauriichan.laylib.reflection.ClassUtil;
 import me.lauriichan.laylib.reflection.JavaAccess;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
 public final class ItemStackIO1_19_R3 extends NbtIO1_19_R3<org.bukkit.inventory.ItemStack, CompoundTag> {
 
     public static final ItemStackIO1_19_R3 ITEM_STACK = new ItemStackIO1_19_R3();
 
-    private final MethodHandle CraftItemStack_handle = JavaAccess.accessFieldGetter(ClassUtil.getField(CraftItemStack.class, "handle"));
+    private static final MethodHandle CraftItemStack_handle = JavaAccess.accessFieldGetter(ClassUtil.getField(CraftItemStack.class, "handle"));
 
     private ItemStackIO1_19_R3() {
         super(org.bukkit.inventory.ItemStack.class, CompoundTag.TYPE);
@@ -40,26 +38,21 @@ public final class ItemStackIO1_19_R3 extends NbtIO1_19_R3<org.bukkit.inventory.
         if (itemStack == null) {
             return ItemStack.EMPTY;
         }
-        if (itemStack instanceof CraftItemStack) {
-            final Object handle = JavaAccess.invoke(itemStack, CraftItemStack_handle);
-            if (handle == null) {
+        if (itemStack instanceof CraftItemStack craftItem) {
+            try {
+                ItemStack handle = (ItemStack) CraftItemStack_handle.invokeExact(craftItem);
+                if (handle == null) {
+                    return ItemStack.EMPTY;
+                }
+                return handle;
+            } catch (Throwable e) {
                 return ItemStack.EMPTY;
             }
-            return (ItemStack) handle;
         }
-        if (itemStack == null || itemStack.getType() == org.bukkit.Material.AIR) {
+        if (itemStack == null || itemStack.getType().isAir()) {
             return ItemStack.EMPTY;
         }
-        @SuppressWarnings("deprecation")
-        final Item item = CraftMagicNumbers.getItem(itemStack.getType(), itemStack.getDurability());
-        if (item == null) {
-            return ItemStack.EMPTY;
-        }
-        final ItemStack stack = new ItemStack(item, itemStack.getAmount());
-        if (itemStack.hasItemMeta()) {
-            CraftItemStack.setItemMeta(stack, itemStack.getItemMeta());
-        }
-        return stack;
+        return CraftItemStack.asNMSCopy(itemStack);
     }
 
 }
