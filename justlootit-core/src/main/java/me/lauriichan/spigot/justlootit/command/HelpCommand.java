8cf8a8d80709b0c59b122d9db112b54b3db22d90
Lauriichan <lauriichan@playuniverse.org>
2023-10-20 22:18:45 +0200

    Updated Help command

--- justlootit-core/src/main/java/me/lauriichan/spigot/justlootit/command/HelpCommand.java
+++ justlootit-core/src/main/java/me/lauriichan/spigot/justlootit/command/HelpCommand.java
@@ -2,6 +2,7 @@ package me.lauriichan.spigot.justlootit.command;
 
 import java.util.List;
 
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import me.lauriichan.laylib.command.Actor;
 import me.lauriichan.laylib.command.CommandManager;
 import me.lauriichan.laylib.command.Node;
@@ -20,69 +21,137 @@ import me.lauriichan.laylib.reflection.ClassUtil;
 }, description = "command.description.justlootit.help")
 public class HelpCommand {
 
-    /*
-     * Help command
-     */
+    private static final String[] EMPTY_ARRAY = new String[0];
 
     @Action("")
-    public void help(final CommandManager commandManager, final Actor<?> actor, @Argument(name = "command") final String command) {
-        final Triple<NodeCommand, Node, String> triple = commandManager.findNode(command);
-        if (triple == null) {
-            actor.sendTranslatedMessage("command.help.command.none", Key.of("command", command));
-            return;
-        }
-        final Node node = triple.getB();
-        final NodeAction action = node.getAction();
-        if (action == null) {
-            if (!node.hasChildren()) {
-                actor.sendTranslatedMessage("command.help.command.empty", Key.of("command", command),
-                    Key.of("description", "$#" + triple.getA().getDescription()));
+    public void help(CommandManager commandManager, Actor<?> actor, @Argument(name = "command", optional = true) String command) {
+        if (command == null || command.isBlank()) {
+            List<NodeCommand> nodeCommands = commandManager.getCommands();
+            StringBuilder builder = new StringBuilder();
+            NodeCommand nodeCommand;
+            int count = 0;
+            for (int index = 0; index < nodeCommands.size(); index++) {
+                nodeCommand = nodeCommands.get(index);
+                if (nodeCommand.isRestricted() && !actor.hasPermission(nodeCommand.getPermission())) {
+                    continue;
+                }
+                if (count++ != 0) {
+                    builder.append('\n');
+                }
+                builder.append(actor.getTranslatedMessageAsString("command.help.tree.format.overview",
+                    Key.of("name", nodeCommand.getName()), Key.of("description", nodeCommand.getDescription())));
+            }
+            if (count == 0) {
+                actor.sendTranslatedMessage("command.help.command.none");
                 return;
             }
-            actor.sendTranslatedMessage("command.help.command.tree", Key.of("command", triple.getC()),
-                Key.of("description", "$#" + triple.getA().getDescription()), Key.of("tree", generateTree(actor, node.getNames())));
+            actor.sendTranslatedMessage("command.help.command.overview", Key.of("tree", builder.toString()));
             return;
         }
-        final StringBuilder argumentBuilder = new StringBuilder(actor.getTranslatedMessageAsString("command.help.argument.format.header"));
-        final List<NodeArgument> argumentList = action.getArguments();
-        boolean found = false;
-        for (int index = 0; index < argumentList.size(); index++) {
-            final NodeArgument argument = argumentList.get(index);
-            if (argument.isProvided()) {
-                continue;
-            }
-            found = true;
-            argumentBuilder.append('\n');
-            final String type = ClassUtil.getClassName(argument.getArgumentType());
-            if (argument.isOptional()) {
-                argumentBuilder.append(actor.getTranslatedMessageAsString("command.help.argument.format.optional",
+        Triple<NodeCommand, Node, String> triple = commandManager.findNode(command);
+        if (triple == null || (triple.getA().isRestricted() && !actor.hasPermission(triple.getA().getPermission()))) {
+            actor.sendTranslatedMessage("command.help.command.unknown", Key.of("command", command));
+            return;
+        }
+        Node node = triple.getB();
+        if (!isSomethingPermitted(actor, node)) {
+            actor.sendTranslatedMessage("command.help.command.unknown", Key.of("command", command));
+            return;
+        }
+        NodeAction action = node.getAction();
+        if (action != null && (!action.isRestricted() || actor.hasPermission(action.getPermission()))) {
+            StringBuilder argumentBuilder = new StringBuilder(actor.getTranslatedMessageAsString("command.help.argument.format.header"));
+            List<NodeArgument> argumentList = action.getArguments();
+            boolean found = false;
+            for (int index = 0; index < argumentList.size(); index++) {
+                NodeArgument argument = argumentList.get(index);
+                if (argument.isProvided()) {
+                    continue;
+                }
+                found = true;
+                argumentBuilder.append('\n');
+                String type = ClassUtil.getClassName(argument.getArgumentType());
+                if (argument.isOptional()) {
+                    argumentBuilder.append(actor.getTranslatedMessageAsString("command.help.argument.format.optional",
+                        Key.of("name", argument.getName()), Key.of("type", type)));
+                    continue;
+                }
+                argumentBuilder.append(actor.getTranslatedMessageAsString("command.help.argument.format.required",
                     Key.of("name", argument.getName()), Key.of("type", type)));
-                continue;
             }
-            argumentBuilder.append(actor.getTranslatedMessageAsString("command.help.argument.format.required",
-                Key.of("name", argument.getName()), Key.of("type", type)));
-        }
-        final String arguments = found ? argumentBuilder.toString()
-            : actor.getTranslatedMessageAsString("command.help.argument.no-arguments");
-        if (node.hasChildren()) {
-            actor.sendTranslatedMessage("command.help.command.tree-executable", Key.of("command", triple.getC()),
-                Key.of("description", "$#" + action.getDescription()), Key.of("arguments", arguments),
-                Key.of("tree", generateTree(actor, node.getNames())));
+            String arguments = found ? argumentBuilder.toString()
+                : actor.getTranslatedMessageAsString("command.help.argument.no-arguments");
+            if (node.hasChildren()) {
+                String[] children = filterChildren(actor, node);
+                if (children.length != 0) {
+                    actor.sendTranslatedMessage("command.help.command.tree-executable", Key.of("command", triple.getC()),
+                        Key.of("description", action.getDescription()), Key.of("arguments", arguments),
+                        Key.of("tree", generateTree(actor, children)));
+                    return;
+                }
+            }
+            actor.sendTranslatedMessage("command.help.command.executable", Key.of("command", triple.getC()),
+                Key.of("description", action.getDescription()), Key.of("arguments", arguments));
             return;
         }
-        actor.sendTranslatedMessage("command.help.command.executable", Key.of("command", triple.getC()),
-            Key.of("description", "$#" + action.getDescription()), Key.of("arguments", arguments));
+        if (node.hasChildren()) {
+            String[] children = filterChildren(actor, node);
+            if (children.length != 0) {
+                actor.sendTranslatedMessage("command.help.command.tree", Key.of("command", triple.getC()),
+                    Key.of("description", triple.getA().getDescription()), Key.of("tree", children));
+                return;
+            }
+        }
+        actor.sendTranslatedMessage("command.help.command.empty", Key.of("command", command),
+            Key.of("description", triple.getA().getDescription()));
+        return;
     }
 
-    private String generateTree(final Actor<?> actor, final String[] names) {
-        final StringBuilder builder = new StringBuilder();
+    private String[] filterChildren(Actor<?> actor, Node node) {
+        if (!node.hasChildren()) {
+            return EMPTY_ARRAY;
+        }
+        ObjectArrayList<String> children = new ObjectArrayList<>();
+        for (String name : node.getNames()) {
+            if (!isSomethingPermitted(actor, node.getNode(name))) {
+                continue;
+            }
+            children.add(name);
+        }
+        if (children.isEmpty()) {
+            return EMPTY_ARRAY;
+        }
+        return children.toArray(String[]::new);
+    }
+
+    private boolean isSomethingPermitted(Actor<?> actor, Node node) {
+        NodeAction action = node.getAction();
+        if (action != null) {
+            if (!action.isRestricted() || actor.hasPermission(action.getPermission())) {
+                return true;
+            }
+            if (!node.hasChildren()) {
+                return false;
+            }
+        } else if (!node.hasChildren()) {
+            return true;
+        }
+        for (String name : node.getNames()) {
+            if (isSomethingPermitted(actor, node.getNode(name))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private String generateTree(Actor<?> actor, String[] names) {
+        StringBuilder builder = new StringBuilder();
         for (int index = 0; index < names.length; index++) {
-            builder.append(actor.getTranslatedMessageAsString("command.help.tree.format", Key.of("name", names[index])));
+            builder.append(actor.getTranslatedMessageAsString("command.help.tree.format.normal", Key.of("name", names[index])));
             if (index + 1 != names.length) {
                 builder.append('\n');
             }
         }
         return builder.toString();
     }
-
 }
--- justlootit-core/src/main/java/me/lauriichan/spigot/justlootit/message/Messages.java
+++ justlootit-core/src/main/java/me/lauriichan/spigot/justlootit/message/Messages.java
@@ -1,9 +1,9 @@
 package me.lauriichan.spigot.justlootit.message;
 
+import me.lauriichan.laylib.localization.MessageProvider;
 import me.lauriichan.laylib.localization.source.Message;
 import me.lauriichan.minecraft.pluginbase.extension.Extension;
 import me.lauriichan.minecraft.pluginbase.message.IMessageExtension;
-import me.lauriichan.minecraft.pluginbase.message.provider.SimpleMessageProvider;
 
 @Extension
 public final class Messages implements IMessageExtension {
@@ -17,9 +17,9 @@ public final class Messages implements IMessageExtension {
      */
 
     @Message(id = "plugin.name", content = "&#2964EAJust&#EA2964Loot&#64EA29It")
-    public static SimpleMessageProvider NAME;
+    public static MessageProvider NAME;
     @Message(id = "plugin.prefix", content = "$#plugin.name &8|&7")
-    public static SimpleMessageProvider PREFIX;
+    public static MessageProvider PREFIX;
 
     /*
      * Commands
@@ -28,12 +28,22 @@ public final class Messages implements IMessageExtension {
     // System (general)
 
     @Message(id = "command.system.actor.not-supported", content = "$#plugin.prefix The command '&c$command&7' can only be excuted by actors of type &c$actorType&7.")
-    public static SimpleMessageProvider COMMAND_SYSTEM_ACTOR_NOT$SUPPORTED;
+    public static MessageProvider COMMAND_SYSTEM_ACTOR_NOT$SUPPORTED;
 
     // Help
 
-    @Message(id = "command.help.command.none", content = "$#plugin.prefix The command '&c$command&7' doesn't exist!")
-    public static SimpleMessageProvider COMMAND_HELP_NONE;
+    @Message(id = "command.help.command.none", content = "$#plugin.prefix There are no commands to list that you are permitted to see!")
+    public static MessageProvider COMMAND_HELP_NONE;
+    @Message(id = "command.help.command.unknown", content = "$#plugin.prefix The command '&c$command&7' doesn't exist!")
+    public static MessageProvider COMMAND_HELP_UNKNOWN;
+    @Message(id = "command.help.command.overview", content = {
+        "&8=< $#plugin.name &8>-< &7Overview",
+        " ",
+        "$tree",
+        " ",
+        "&8=< $#plugin.name &8>-< &7Overview"
+    })
+    public static MessageProvider COMMAND_HELP_OVERVIEW;
     @Message(id = "command.help.command.empty", content = {
         "&8=< $#plugin.name &8>-< &7$command",
         " ",
@@ -41,7 +51,7 @@ public final class Messages implements IMessageExtension {
         " ",
         "&8=< $#plugin.name &8>-< &7$command"
     })
-    public static SimpleMessageProvider COMMAND_HELP_EMPTY;
+    public static MessageProvider COMMAND_HELP_EMPTY;
     @Message(id = "command.help.command.tree", content = {
         "&8=< $#plugin.name &8>-< &7$command",
         " ",
@@ -52,7 +62,7 @@ public final class Messages implements IMessageExtension {
         " ",
         "&8=< $#plugin.name &8>-< &7$command"
     })
-    public static SimpleMessageProvider COMMAND_HELP_TREE;
+    public static MessageProvider COMMAND_HELP_TREE;
     @Message(id = "command.help.command.executable", content = {
         "&8=< $#plugin.name &8>-< &7$command",
         " ",
@@ -64,7 +74,7 @@ public final class Messages implements IMessageExtension {
         " ",
         "&8=< $#plugin.name &8>-< &7$command"
     })
-    public static SimpleMessageProvider COMMAND_HELP_EXECUATABLE;
+    public static MessageProvider COMMAND_HELP_EXECUATABLE;
     @Message(id = "command.help.command.tree-executable", content = {
         "&8=< $#plugin.name &8>-< &7$command",
         " ",
@@ -77,16 +87,18 @@ public final class Messages implements IMessageExtension {
         " ",
         "&8=< $#plugin.name &8>-< &7$command"
     })
-    public static SimpleMessageProvider COMMAND_HELP_TREE$EXECUTABLE;
+    public static MessageProvider COMMAND_HELP_TREE$EXECUTABLE;
     @Message(id = "command.help.argument.no-arguments", content = "There are no arguments for this command")
-    public static SimpleMessageProvider COMMAND_HELP_ARGUMENT_NO$ARGUMENTS;
+    public static MessageProvider COMMAND_HELP_ARGUMENT_NO$ARGUMENTS;
     @Message(id = "command.help.argument.format.header", content = "&7[required] <optional>")
-    public static SimpleMessageProvider COMMAND_HELP_ARGUMENT_FORMAT_HEADER;
+    public static MessageProvider COMMAND_HELP_ARGUMENT_FORMAT_HEADER;
     @Message(id = "command.help.argument.format.required", content = "&8[&7$type&8] &7$name")
-    public static SimpleMessageProvider COMMAND_HELP_ARGUMENT_FORMAT_REQUIRED;
+    public static MessageProvider COMMAND_HELP_ARGUMENT_FORMAT_REQUIRED;
     @Message(id = "command.help.argument.format.optional", content = "&8<&7$type&8> &7$name")
-    public static SimpleMessageProvider COMMAND_HELP_ARGUMENT_FORMAT_OPTIONAL;
-    @Message(id = "command.help.tree.format", content = "&8- &7$name")
-    public static SimpleMessageProvider COMMAND_HELP_TREE_FORMAT;
+    public static MessageProvider COMMAND_HELP_ARGUMENT_FORMAT_OPTIONAL;
+    @Message(id = "command.help.tree.format.normal", content = "&8- &7$name")
+    public static MessageProvider COMMAND_HELP_TREE_FORMAT_NORMAL;
+    @Message(id = "command.help.tree.format.overview", content = "&8/town &7$name &8- &7$description")
+    public static MessageProvider COMMAND_HELP_TREE_FORMAT_OVERVIEW;
 
 }
