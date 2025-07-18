34a6df56ffdba065b59445139e0021cd8b0209ad
Lauriichan <lauriichan@playuniverse.org>
2025-07-18 19:45:04 +0200

    Add support for 1.21.8 on Paper (update version to 2.6.3)

--- justlootit-platform-api/src/main/java/me/lauriichan/spigot/justlootit/platform/util/MinecraftToPackageVersion.java
+++ justlootit-platform-api/src/main/java/me/lauriichan/spigot/justlootit/platform/util/MinecraftToPackageVersion.java
@@ -11,7 +11,7 @@ public enum MinecraftToPackageVersion {
     v1_21_R2(of(1, 21, 3)),
     v1_21_R3(of(1, 21, 4)),
     v1_21_R4(of(1, 21, 5)),
-    v1_21_R5(of(1, 21, 6), of(1, 21, 7));
+    v1_21_R5(of(1, 21, 6), of(1, 21, 8));
 
     private static final MinecraftToPackageVersion[] values = MinecraftToPackageVersion.values();
 
