f3f38755882059e30bab13df68a0863184f2fdb6
Lauriichan <lauriichan@playuniverse.org>
2026-04-18 11:10:58 +0200

    Version fix for folia

--- justlootit-platform-folia/src/main/java/me/lauriichan/spigot/justlootit/platform/folia/FoliaVersion.java
+++ justlootit-platform-folia/src/main/java/me/lauriichan/spigot/justlootit/platform/folia/FoliaVersion.java
@@ -11,20 +11,28 @@ public final class FoliaVersion implements IVersion {
     public static final FoliaVersion INSTANCE = new FoliaVersion();
 
     public final SimpleVersion minecraftVersion = SimpleVersion.of(Bukkit.getServer().getMinecraftVersion());
-    public final String packageVersion;
+    public final String packageVersion, coreVersion;
     public final String craftbukkitPackage = Bukkit.getServer().getClass().getPackage().getName() + ".%s";
 
     private FoliaVersion() {
         String pkgVer = MinecraftToPackageVersion.getPackageVersion(minecraftVersion);
+        String coreVer = pkgVer;
         if (pkgVer == null && minecraftVersion.major() >= 26) {
-            pkgVer = "v26_1";
+            pkgVer = "paper.v26_1";
+            coreVer = "26_1";
         }
         this.packageVersion = pkgVer;
+        this.coreVersion = coreVer;
     }
 
     @Override
     public String packageVersion() {
         return packageVersion;
+    }
+
+    @Override
+    public String coreVersion() {
+        return coreVersion;
     }
 
     @Override
